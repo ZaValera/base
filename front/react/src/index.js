7eddd802d8cf662c89938330e100476aa0167c3d
Valeriy Zaitsev <valeriy.zaitsev@corp.mail.ru>
2018-08-06 22:46:11 +0300

    remove trash

--- front/react/src/index.js
+++ front/react/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter, Route, Switch, Link, HashRouter} from 'react-router-dom';
 import {App} from './components/App/App';
-import * as $ from 'jquery';
 
 ReactDOM.render(
     <BrowserRouter>
@@ -10,10 +9,3 @@ ReactDOM.render(
     </BrowserRouter>,
     document.getElementById('app'),
 );
-
-window.$ = $;
-
-$.ajax({
-    url: '/api/v1/test',
-    type: 'POST',
-});
